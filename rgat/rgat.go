d4df6028a6da95109beebd1e528a7424277ac54e
Iago Tito Oliveira <iago.oliveira@ccc.ufcg.edu.br>
2021-09-07 18:40:24 -0300

    create config subcommand stub

--- rgat/rgat.go
+++ rgat/rgat.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,7 +11,29 @@ const HelpText = `usage: rgat <command> [<args>]
 
 Avaliable commands:
 
+  config        Crate a configuration file
+
 'rgat help' list available subcommands`
+
+const ConfigHelpText = `Name
+  rgat config - Configure access information
+
+Synopsis
+  rgat config [-username <username>] [-password <password>] [-help]
+
+Description
+  Configure the access information.
+  Run 'rgat config' without options to open an interactive configuration.
+
+Options
+  --username
+    Username
+
+  --passowrd
+    Password
+
+  --help
+    Show help text`
 
 func check(err error) {
     if err != nil {
@@ -18,10 +41,20 @@ func check(err error) {
     }
 }
 
+type config struct {
+    username string
+    password string
+}
+
 func main() {
     if len(os.Args) < 2 {
         fmt.Println(HelpText)
     }
+
+    configCmd := flag.NewFlagSet("config", flag.ExitOnError)
+    configUsername := configCmd.String("username", "", "Username")
+    configPassword := configCmd.String("passowrd", "", "Password")
+    configHelp := configCmd.Bool("help", false, "Help")
 
     switch os.Args[1] {
 
@@ -30,7 +63,17 @@ func main() {
             fmt.Printf("Unknow args for help: %v\n", os.Args[2])
         }
         fmt.Println(HelpText)
+    case "config":
+        configCmd.Parse(os.Args[2:])
+        if *configHelp {
+            fmt.Println(ConfigHelpText)
+        }
+        setConfig(config{*configUsername, *configPassword})
     default:
         fmt.Println(HelpText)
     }
+}
+
+func setConfig(conf config) {
+    fmt.Println(conf)
 }
