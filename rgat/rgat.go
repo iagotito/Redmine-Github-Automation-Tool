064e608571e312d72f2eacd18ec49be74e38c3c1
Iago Tito Oliveira <iago.oliveira@ccc.ufcg.edu.br>
2021-09-08 00:11:12 -0300

    create issues on Redmine based on yaml files

--- rgat/rgat.go
+++ rgat/rgat.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"reflect"
 
 	"gopkg.in/yaml.v3"
 )
@@ -50,7 +55,10 @@ Description
 
 Options
   --read-yaml <path to yaml file>
-    Read a sprint info from a YAML file and print on the screen
+    Read a sprint info from a YAML file and creates issues on Redmine
+
+  --config-flie <path to config yaml file>
+    Config file with acess credentials and project url
 
   --help
     Show help text`
@@ -59,11 +67,6 @@ func check(err error) {
     if err != nil {
         log.Fatal(err)
     }
-}
-
-type config struct {
-    username string
-    password string
 }
 
 func main() {
@@ -78,7 +81,8 @@ func main() {
 
     redmineCmd := flag.NewFlagSet("redmine", flag.ExitOnError)
     redmineHelp := redmineCmd.Bool("help", false, "Help")
-    redmineReadYaml := redmineCmd.String("read-yaml", "", "Read YAML file")
+    redmineReadYaml := redmineCmd.String("read-yaml", "", "Read sprint YAML file")
+    redmineConfigFile := redmineCmd.String("config-file", "", "Path to config YAML file")
 
     switch os.Args[1] {
 
@@ -93,7 +97,7 @@ func main() {
             fmt.Println(ConfigHelpText)
             return
         }
-        setConfig(config{*configUsername, *configPassword})
+        setConfig(Config{"http://project.com", *configUsername, *configPassword})
     case "redmine":
         redmineCmd.Parse(os.Args[2:])
         if *redmineHelp {
@@ -103,20 +107,29 @@ func main() {
         if *redmineReadYaml != "" {
             sprint, err := readSprintYaml(*redmineReadYaml)
             check(err)
-            fmt.Println(sprint)
+            config, err := readConfigYaml(*redmineConfigFile)
+            check(err)
+
+            createSprintIssues(sprint, config)
         }
     default:
         fmt.Println(HelpText)
     }
 }
 
-func setConfig(conf config) {
+func setConfig(conf Config) {
     fmt.Println(conf)
 }
 
+type Config struct {
+    ProjectUrl string `yaml:"project-url"`
+    Username string `yaml:"username"`
+    Password string `yaml:"password"`
+}
+
 type Sprint struct {
-    SprintName string `yaml:"sprint"`
-    Tasks []Issue `yaml:"issues"`
+    SprintNum string `yaml:"sprint"`
+    Issues []Issue `yaml:"issues"`
 }
 
 type Issue struct {
@@ -124,6 +137,24 @@ type Issue struct {
     Description string `yaml:"description"`
     Subissues []Issue `yaml:"subissues"`
     EstimatedHours float32 `yaml:"estimated-hours"`
+}
+
+var yamlToJsonNames = map[string]string{
+    "Subject": "subject",
+    "Description": "description",
+    "EstimatedHours": "estimated-hours",
+    "Subissues": "sub-issues",
+}
+
+func readConfigYaml(yamlPath string) (Config, error) {
+    yfile, err := ioutil.ReadFile(yamlPath)
+    if err != nil { return Config{}, err }
+
+    var config Config
+    err = yaml.Unmarshal(yfile, &config)
+    check(err)
+
+    return config, nil
 }
 
 func readSprintYaml(yamlPath string) (Sprint, error) {
@@ -135,4 +166,56 @@ func readSprintYaml(yamlPath string) (Sprint, error) {
     check(err)
 
     return sprint, nil
+}
+
+func createSprintIssues(sprint Sprint, config Config) {
+    //sprintNum := sprint.SprintNum
+
+    fmt.Println(sprint.Issues)
+    for i := 0; i < len(sprint.Issues); i++ {
+        issueBytes, err := makeIssueJsonBytes(&sprint.Issues[i])
+        check(err)
+
+        client := http.Client{}
+
+        body := bytes.NewReader(issueBytes)
+        url := fmt.Sprintf("%v/issues.json", config.ProjectUrl)
+        req, err := http.NewRequest("POST", url, body)
+        check(err)
+
+        req.Header.Add("Content-Type", "application/json")
+        req.SetBasicAuth(config.Username, config.Password)
+
+        res, err := client.Do(req)
+        check(err)
+        defer res.Body.Close()
+
+        resBody, err := io.ReadAll(res.Body)
+        check(err)
+
+        fmt.Printf("Status: %d\n", res.StatusCode)
+        fmt.Printf("Body: %s\n", string(resBody))
+    }
+}
+
+// TODO: transform it into a Issue "method"
+func makeIssueJsonBytes(issue *Issue) ([]byte, error) {
+    issueJson := make(map[string]map[string]interface {})
+    issueMap := make(map[string]interface {})
+    issueJson["issue"] = issueMap
+
+    s := reflect.ValueOf(issue).Elem()
+    for i := 0; i < s.NumField(); i++ {
+        fieldName := yamlToJsonNames[s.Type().Field(i).Name]
+        fieldValue := s.Field(i).Interface()
+        issueMap[fieldName] = fieldValue
+    }
+
+    issueJsonBytes, err := json.Marshal(issueJson)
+    if err != nil {
+        return nil, err
+    }
+    ioutil.WriteFile("sprint.json", issueJsonBytes, os.ModePerm)
+
+    return issueJsonBytes, nil
 }
